9a6b2b29dafba53da8316db8c7b7214dbc06ae6e
Rodrigo Jorge <rodrigodoj@gmail.com>
2021-04-06 18:03:28 -0300

    projeto criado

---
+++ src/main/java/com/rodrigodojo/applicationsbjpa/ApplicationSbjpaApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigodojo.applicationsbjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationSbjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationSbjpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigodojo/applicationsbjpa/ApplicationSbjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigodojo.applicationsbjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationSbjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
